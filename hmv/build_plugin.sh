4ce02f66cd301a9fc3fc071eea31d6ca03cbbeba
Tamas Kalman <hello@tamaskalman.com>
2018-01-06 19:11:18 +0100

    Made plugin installer script more generic

--- hmv/build_plugin.sh
+++ hmv/build_plugin.sh
@@ -1,11 +1,14 @@
-#!/bin/zsh
+#!/bin/bash
 currenttime=$(date '+%Y-%m-%d_%H-%M-%S')
 builddir=$(pwd)'/../../build_'$currenttime
+plugindir="~/.qgis2/python/plugins/hmv/"
 mkdir $builddir
 cp *.py $builddir
 cp metadata.txt $builddir
 cp *.svg $builddir
 if [ "$#" -eq 1 ] && [ "$1" = "-i" ]; then
-    rm -fr ~/.qgis2/python/plugins/hmv/*
-    cp -R "$builddir/"* ~/.qgis2/python/plugins/hmv/
+	if [ ! -d $plugindir ] && mkdir $plugindir
+
+	rm -fr ~/.qgis2/python/plugins/hmv/*
+	cp -R "$builddir/"* ~/.qgis2/python/plugins/hmv/
 fi
