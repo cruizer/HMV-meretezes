f2bd75f1b6566f978176ee5547d21e6e07f36af0
Tamas Kalman <hello@tamaskalman.com>
2018-11-08 23:11:17 +0100

    Added summary comment, added spaces around blocks

--- hmv/build_plugin.sh
+++ hmv/build_plugin.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
+# SCRIPT SUMMARY
+# 1. Creates an INI file if it doesn't exist
+# 2. Stores log and working directory in INI config
+# 3. Copies the necessary plugin files to the build directory
+# 4. Creates the QGIS plugin directory if it doesn't exist
+# 5. Copies the plugin files from the build to the plugin directory
 currenttime=$(date '+%Y-%m-%d_%H-%M-%S')
 projdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
+
 if [ ! -f "$projdir/hmv_meretezes_plugin.py" ];then
     echo "ERROR: this script needs to be located in the project directory!"
     exit 1
 fi
+
 builddir="$projdir"'/../build/build_'$currenttime
 plugindir=~/.qgis2/python/plugins/hmv/
+
 if [ ! -f "${projdir}/hmv.ini" ]; then
 	echo "No ini file, creating..."
 	echo '[hmv]' > "${projdir}"/hmv.ini
@@ -24,13 +33,15 @@ if [ ! -f "${projdir}/hmv.ini" ]; then
 	echo "logdir=$logpath" >> "${projdir}"/hmv.ini
 	echo "workdir=$workdir" >> "${projdir}"/hmv.ini
 fi
-mkdir $builddir
+
+mkdir -p $builddir
 cp -v "${projdir}"/*.py $builddir
 cp -v "${projdir}"/metadata.txt $builddir
 cp -v "${projdir}"/*.svg $builddir
 cp -v "${projdir}"/*.ini $builddir
+
 if [ "$#" -eq 1 ] && [ "$1" = "-i" ]; then
-	[ ! -d "$plugindir" ] && mkdir "$plugindir"
+	[ ! -d "$plugindir" ] && mkdir -p "$plugindir"
 	rm -frv "${plugindir}/"*
 	cp -v -R "${builddir}/"* "$plugindir"
 fi
