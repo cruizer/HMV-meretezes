7811f8a67fb297ccfe3fa7abc1a85d8613b2bab1
Tamas Kalman <hello@tamaskalman.com>
2019-01-22 22:22:06 +0100

    Completed ENG-HUN translation of plugin UI static text

---
+++ i18n/hmv_meretezes_hu_HU.ts
@@ -0,0 +1,256 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0">
+<context>
+    <name>HmvWidget</name>
+    <message>
+        <location filename="../hmv/hmv_widget.py" line="395"/>
+        <source>Domestic Hot Water System Sizing</source>
+        <translation type="unfinished">HMV Méretezés</translation>
+    </message>
+    <message>
+        <location filename="../hmv/hmv_widget.py" line="396"/>
+        <source>Reset</source>
+        <translation type="unfinished">Visszaállítás</translation>
+    </message>
+    <message>
+        <location filename="../hmv/hmv_widget.py" line="397"/>
+        <source>Save</source>
+        <translation type="unfinished">Mentés</translation>
+    </message>
+    <message>
+        <location filename="../hmv/hmv_widget.py" line="398"/>
+        <source>Layers Selection</source>
+        <translation type="unfinished">Réteg Választás</translation>
+    </message>
+    <message>
+        <location filename="../hmv/hmv_widget.py" line="399"/>
+        <source>Node Layer</source>
+        <translation type="unfinished">Elemek Réteg</translation>
+    </message>
+    <message>
+        <location filename="../hmv/hmv_widget.py" line="400"/>
+        <source>Pipe Layer</source>
+        <translation type="unfinished">Csőhálózat Réteg</translation>
+    </message>
+    <message>
+        <location filename="../hmv/hmv_widget.py" line="401"/>
+        <source>Format</source>
+        <translation type="unfinished">Formázás</translation>
+    </message>
+    <message>
+        <location filename="../hmv/hmv_widget.py" line="402"/>
+        <source>Base Data</source>
+        <translation type="unfinished">Kiindulási Adatok</translation>
+    </message>
+    <message>
+        <location filename="../hmv/hmv_widget.py" line="403"/>
+        <source>Density</source>
+        <translation type="unfinished">Sűrűség</translation>
+    </message>
+    <message>
+        <location filename="../hmv/hmv_widget.py" line="404"/>
+        <source>Specific Heat</source>
+        <translation type="unfinished">Fajhő</translation>
+    </message>
+    <message>
+        <location filename="../hmv/hmv_widget.py" line="405"/>
+        <source>&#xce;&#x94;&#xcf;&#x91;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../hmv/hmv_widget.py" line="406"/>
+        <source>Pipe Velocity</source>
+        <translation type="unfinished">Csősebesség</translation>
+    </message>
+    <message>
+        <location filename="../hmv/hmv_widget.py" line="407"/>
+        <source>kg/m3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../hmv/hmv_widget.py" line="408"/>
+        <source>J/kgK</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../hmv/hmv_widget.py" line="409"/>
+        <source>K</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../hmv/hmv_widget.py" line="410"/>
+        <source>m/s</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../hmv/hmv_widget.py" line="411"/>
+        <source>New DHW System Layer</source>
+        <translation type="unfinished">Új HMV Réteg</translation>
+    </message>
+    <message>
+        <location filename="../hmv/hmv_widget.py" line="412"/>
+        <source>Layer Name</source>
+        <translation type="unfinished">Réteg Név</translation>
+    </message>
+    <message>
+        <location filename="../hmv/hmv_widget.py" line="413"/>
+        <source>Database File Name</source>
+        <translation type="unfinished">Adatbázis Filenév</translation>
+    </message>
+    <message>
+        <location filename="../hmv/hmv_widget.py" line="414"/>
+        <source>Layer Type</source>
+        <translation type="unfinished">Réteg Típus</translation>
+    </message>
+    <message>
+        <location filename="../hmv/hmv_widget.py" line="415"/>
+        <source>Add to Layers Panel?</source>
+        <translation type="unfinished">Hozzáadás a Réteg Listához?</translation>
+    </message>
+    <message>
+        <location filename="../hmv/hmv_widget.py" line="416"/>
+        <source>szakaszok</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../hmv/hmv_widget.py" line="417"/>
+        <source>elemek</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../hmv/hmv_widget.py" line="418"/>
+        <source>Layers Working Directory</source>
+        <translation type="unfinished">Réteg Munkakönyvtár</translation>
+    </message>
+    <message>
+        <location filename="../hmv/hmv_widget.py" line="419"/>
+        <source>Create</source>
+        <translation type="unfinished">Létrehozás</translation>
+    </message>
+    <message>
+        <location filename="../hmv/hmv_widget.py" line="420"/>
+        <source>Settings</source>
+        <translation type="unfinished">Beállítások</translation>
+    </message>
+    <message>
+        <location filename="../hmv/hmv_widget.py" line="421"/>
+        <source>+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../hmv/hmv_widget.py" line="442"/>
+        <source>-</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../hmv/hmv_widget.py" line="423"/>
+        <source>Add Size</source>
+        <translation type="unfinished">Méret Hozzáadása</translation>
+    </message>
+    <message>
+        <location filename="../hmv/hmv_widget.py" line="424"/>
+        <source>Pipe Sizes</source>
+        <translation type="unfinished">Csőméretek</translation>
+    </message>
+    <message>
+        <location filename="../hmv/hmv_widget.py" line="425"/>
+        <source>Validate Network</source>
+        <translation type="unfinished">Hálózat Ellenőrzés</translation>
+    </message>
+    <message>
+        <location filename="../hmv/hmv_widget.py" line="426"/>
+        <source>Status:</source>
+        <translation type="unfinished">Státusz:</translation>
+    </message>
+    <message>
+        <location filename="../hmv/hmv_widget.py" line="427"/>
+        <source>Not Validated</source>
+        <translation type="unfinished">Nem Ellenőrzött</translation>
+    </message>
+    <message>
+        <location filename="../hmv/hmv_widget.py" line="428"/>
+        <source>Total Elements</source>
+        <translation type="unfinished">Összes Elem Száma</translation>
+    </message>
+    <message>
+        <location filename="../hmv/hmv_widget.py" line="431"/>
+        <source>0</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../hmv/hmv_widget.py" line="430"/>
+        <source>Failed Elements</source>
+        <translation type="unfinished">Hibás Elemek Száma</translation>
+    </message>
+    <message>
+        <location filename="../hmv/hmv_widget.py" line="432"/>
+        <source>Failed Elements List (Loose Connection)</source>
+        <translation type="unfinished">Hibás Elemek (csatlakozás nem megfelelő)</translation>
+    </message>
+    <message>
+        <location filename="../hmv/hmv_widget.py" line="433"/>
+        <source>Network Validation</source>
+        <translation type="unfinished">Hálózat Ellenőrzés</translation>
+    </message>
+    <message>
+        <location filename="../hmv/hmv_widget.py" line="434"/>
+        <source>Partial Results</source>
+        <translation type="unfinished">Részeredmények</translation>
+    </message>
+    <message>
+        <location filename="../hmv/hmv_widget.py" line="435"/>
+        <source>Required Pump Flow</source>
+        <translation type="unfinished">Szivattyú Által Biztosítandó Térfogatáram</translation>
+    </message>
+    <message>
+        <location filename="../hmv/hmv_widget.py" line="436"/>
+        <source>Total HeatLoss of Forward Network</source>
+        <translation type="unfinished">Teljes Előremenő Hálózat Hővesztesége</translation>
+    </message>
+    <message>
+        <location filename="../hmv/hmv_widget.py" line="437"/>
+        <source>Analyze</source>
+        <translation type="unfinished">Analízis Futtatása</translation>
+    </message>
+    <message>
+        <location filename="../hmv/hmv_widget.py" line="438"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; color:#e82517;&quot;&gt;H&#xc3;&#xa1;l&#xc3;&#xb3;zat ellen&#xc5;&#x91;rz&#xc3;&#xa9;s sz&#xc3;&#xbc;ks&#xc3;&#xa9;ges! M&#xc3;&#xa9;g nem t&#xc3;&#xb6;rt&#xc3;&#xa9;nt h&#xc3;&#xa1;l&#xc3;&#xb3;zat ellen&#xc5;&#x91;rz&#xc3;&#xa9;s, vagy a h&#xc3;&#xa1;l&#xc3;&#xb3;zat megv&#xc3;&#xa1;ltozott.&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../hmv/hmv_widget.py" line="440"/>
+        <source>dm3/h</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../hmv/hmv_widget.py" line="441"/>
+        <source>m3/sec</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../hmv/hmv_widget.py" line="443"/>
+        <source>W</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../hmv/hmv_widget.py" line="444"/>
+        <source>Show Results</source>
+        <translation type="unfinished">Eredmények Mutatása</translation>
+    </message>
+    <message>
+        <location filename="../hmv/hmv_widget.py" line="445"/>
+        <source>Loop Pressure and Choke Results</source>
+        <translation type="unfinished">Körönkénti Nyomás és Fojtás Eredmények</translation>
+    </message>
+    <message>
+        <location filename="../hmv/hmv_widget.py" line="446"/>
+        <source>Flow Results</source>
+        <translation type="unfinished">Áramlás Eredmények</translation>
+    </message>
+    <message>
+        <location filename="../hmv/hmv_widget.py" line="447"/>
+        <source>Analysis</source>
+        <translation type="unfinished">Analízis</translation>
+    </message>
+</context>
+</TS>
